70a64fb9249d3a8c428a8dc69547f51334e1bea6
git-alice <art.oxbow@gmail.com>
2020-04-17 00:29:33 +0300

    Added display_obj

--- tatarinov/utils/jupyter.py
+++ tatarinov/utils/jupyter.py
@@ -1,9 +1,24 @@
 from IPython.display import display, Markdown
-from typing import List, Any
+from typing import Any, Sequence
 
-def display_list(l: List[Any], spaces: bool = False):
+
+def display_list(l: Sequence[Any], spaces: bool = False):
+    """
+
+    @param l: Sequence[Any], Последовательность объектов
+    @param spaces: bool, Нужно ли разделительное пространство
+    @return: None
+    """
     for x in l:
         if spaces:
             display(Markdown('---'))
             # print('@@@'.center('_', 50))
         display(x)
+
+
+def display_obj(obj: Any) -> None:
+    """
+
+    @type obj: object
+    """
+    display(obj)
