a8c39c41bcf408ffee600f722daef9410d5ff087
git-alice <art.oxbow@gmail.com>
2020-04-18 17:29:38 +0300

    Deleted web.utils, web.tmp

--- web/utils.py
+++
@@ -1,9 +0,0 @@
-import cloudpickle as pickle
-from pathlib import Path
-
-data_dir = Path('/home/alice/Documents/Course/tatarinov_equation/triangle_platform/data')
-
-def load(name):
-    with open(data_dir.joinpath(f'{name}.pickle'), 'rb') as f:
-        data = pickle.load(f)
-    return data
