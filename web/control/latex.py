f620d626134bad46929f59c4227b0d987095d134
git-alice <art.oxbow@gmail.com>
2020-04-18 16:55:38 +0300

    Added funcs equation_by_name, all_equation_names

--- web/control/latex.py
+++ web/control/latex.py
@@ -1,17 +1,35 @@
 from sympy import latex
 import timeago
 from datetime import datetime
+from typing import List
 
+from tatarinov.database.pickle import Pickled
 from tatarinov.database.singleton import db
 
 db.set_root('../data/body_with_F/')
 
 
-def all_equations():
+def all_equations() -> List[Pickled]:
     data = db.load_all()
     for item in data:
         item.latex = latex(item.obj)
         item.str_when_saved = item.when_saved.strftime("%Y-%m-%d %H:%M:%S")
         item.str_saved_time_ago = timeago.format(item.when_saved, datetime.now())
-
     return data
+
+
+def equation_by_name(name: str) -> Pickled:
+    """
+
+    @param name: str, Имя объекта
+    @return: Pickled, Объект
+    """
+    return db.load(name)
+
+
+def all_equation_names() -> List[str]:
+    """
+
+    @return: List[str], Список имен
+    """
+    return db.get_all_names()
