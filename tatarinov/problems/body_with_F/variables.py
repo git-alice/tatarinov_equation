fb9ed15ea24be6c695c16004b85055b692e4aea2
git-alice <art.oxbow@gmail.com>
2020-04-18 12:40:49 +0300

    Fixed body_with_F problem variables

--- tatarinov/problems/body_with_F/variables.py
+++ tatarinov/problems/body_with_F/variables.py
@@ -1,22 +1,31 @@
 from sympy import IndexedBase, symbols, Matrix, Idx
 from sympy import cos, sin
+from sympy.physics.vector import dynamicsymbols
 
-L, m, x, y, t, alpha = symbols('L, m, x, y, t, alpha')
-nu1, nu2, nu3 = symbols('nu1, nu2, nu3')
+# L, m, x, y, t, alpha = symbols('L, m, x, y, t, alpha')
+p = IndexedBase('p')
+J = IndexedBase('J')
+v = IndexedBase('v')
+omega = IndexedBase('omega')
+
+k = symbols('k', cls=Idx)
+
+x, y, alpha = dynamicsymbols('x, y , alpha')
+
+t, xi, eta = symbols('t, xi, eta')
+L, m = symbols('L, m')
 W, T = symbols('W, T')
 s = symbols('s')
-P = IndexedBase('P')
-p = IndexedBase('p')
-omega = IndexedBase('omega')
-v = IndexedBase('v')
-Q = IndexedBase('Q')
-k = symbols('k', cls=Idx)
-J = IndexedBase('J')
+nu1, nu2, nu3 = dynamicsymbols('nu1, nu2, nu3')
 
-xi, eta = symbols('xi, eta')
+
 e = {
     'x': Matrix([1, 0]),
     'y': Matrix([0, 1]),
     'xi': Matrix([cos(alpha), sin(alpha)]),
     'eta': Matrix([-sin(alpha), cos(alpha)])
 }
+
+r = {}
+r['s'] = x * e['x'] + y * e['y']
+r['p'] = r['s'] + xi * e['xi'] + eta * e['eta']
