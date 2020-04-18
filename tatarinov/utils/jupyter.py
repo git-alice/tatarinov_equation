a733b82dacfd988534b24b91abff3fd9d8c284ab
git-alice <art.oxbow@gmail.com>
2020-04-18 12:38:42 +0300

    Added display_obj, Updated debug_display

--- tatarinov/utils/jupyter.py
+++ tatarinov/utils/jupyter.py
@@ -16,9 +16,25 @@ def display_list(l: Sequence[Any], spaces: bool = False):
         display(x)
 
 
-def display_obj(obj: Any) -> None:
+def display_obj(obj: Any, description: str = None) -> None:
     """
 
-    @type obj: object
+    @param obj: Any
+    @param description: str, Описание вывода
+    @return: None
     """
+    display(Markdown(f'__{description}__'))
     display(obj)
+
+
+def debug_display(obj: Any, description: str = None) -> None:
+    """
+
+    @param obj: Any, Любой объект для промежуточного вывода
+    @param description: str, Описание вывода
+    @return: None
+    """
+    display(Markdown('_dubug output ↓_'))
+    display(Markdown(f'__{description}__'))
+    display(obj)
+    display(Markdown('---'))
