826832e8f98bd63316d9201c1fd41c3f08129cc5
git-alice <art.oxbow@gmail.com>
2020-04-18 17:28:24 +0300

    Added list of equations to routes

--- web/routes.py
+++ web/routes.py
@@ -2,7 +2,7 @@
 from flask import current_app as app
 from flask import render_template
 
-from web.control.latex import all_equations
+from web.control.latex import all_equations, equation_by_name, all_equation_names
 
 @app.route('/')
 def home():
@@ -19,7 +19,12 @@ def latex():
     # load('S')
     # with open('/home/alice/Documents/Course/tatarinov_equation/web/tmp.txt', 'r') as file:
     #     data = file.read()
+    names = all_equation_names()
     data = all_equations()
-    return render_template('latex.html', data=data)
+    return render_template('latex.html', names=names, data=data)
 
-
+@app.route('/latex/<objname>')
+def latex_by_name(objname):
+    names = all_equation_names()
+    data = equation_by_name(objname)
+    return render_template('latex.html', names=names, data=data)
