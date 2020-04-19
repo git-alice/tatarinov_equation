064b6205891f7f5cdd78d479a208a8d436c6b5d4
git-alice <art.oxbow@gmail.com>
2020-04-19 14:57:50 +0300

    Init triangular_platform in tatarinov.problems

---
+++ tatarinov/problems/triangular_platform/functions.py
@@ -0,0 +1,94 @@
+from tatarinov.utils.pprint import cpprint
+
+# from tatarinov.problems.triangular_platform.structure import *
+
+def fkey(dict_key):
+    '''Нужна чтобы ключами словаря делать словарь, почему таак приходится делать не очень ясно
+       https://stackoverflow.com/questions/13264511/typeerror-unhashable-type-dict'''
+    if isinstance(dict_key, dict):
+        return frozenset((key, fkey(value)) for key, value in dict_key.items())
+    elif isinstance(dict_key, list):
+        return tuple(fkey(value) for value in dict_key)
+    return dict_key
+
+
+def scalar(a, b):
+    """скалярное произведение"""
+    return (a.dot(b))
+
+
+def cross(a, b):
+    """векторное произведение"""
+    return (a.cross(b))
+
+
+def vec_invert(vec):
+    """из вектора AB получает BA
+       (вектор это лямбда функция)"""
+    return vec * (-1);
+
+
+def intersection(dict_one, dict_two, key='where'):
+    """Находит пересечения значений по ключу в 2 словарях.
+       Нужно для того чтобы писать формулу Эйлера и смотреть, принадлежат ли точки одному тело или нет"""
+    return list(set(dict_one[key]) & set(dict_two[key]))
+
+
+def vec_by_2dots(dot_one, dot_two):
+    """по точкам A, B  вернёт  AB или -BA"""
+    vec_name = dot_one['name'] + dot_two['name']
+    try:
+        res_vec = getattr(structure, vec_name)
+    #         print(vec_name)
+    except Exception as e:
+        try:
+            res_vec = -getattr(structure, vec_name[::-1])(i)
+        #             print('-' + vec_name[::-1])
+        # print('Обратный вектору {}?'.format(vec_name))
+        except Exception as e:
+            cpprint('Такого вектора не задано', 'red')
+            print(e)
+    #     print(res_vec(0))
+    return res_vec
+
+
+def subs_delta(eq):
+    return lambda i: eq(i).subs(
+        Derivative(alpha, t), delta['alpha']).subs(
+        Derivative(psi[i], t), delta['psi'][i]).subs(
+        Derivative(theta[i], t), delta['theta'][i])
+
+
+def euler(dot_one, dot_two):
+    '''уравнение эйлера по 2 точкам для первой точки'''
+    if dot_one == dot_two:
+        cpprint('Уравнениу Эйлера для одной точки', 'red')
+    return lambda i: v[dot_two['name']](i) + cross(omega[intersection(dot_one, dot_two)[0]](i),
+                                                   -vec_by_2dots(dot_one, dot_two)(i))
+
+# TODO: Удалить
+
+def dalamber(mass, velocity, F, delta_r, K, M, omega_delta):
+    '''Д'Аламбера - Лагранжа'''
+    dalamber = lambda i: scalar((mass * velocity(i).diff(t) - \
+                                 F(i)), delta_r(i)) + \
+                         scalar(K(i).diff(t) - \
+                                M(i), omega_delta(i))
+    return dalamber
+
+
+def dalamber_subs(obj, eq):
+    """ subs delta_psi delta_theta """
+    return lambda i: obj(i). \
+        subs(delta['psi'][i], eq['diff(psi)'](i)). \
+        subs(delta['theta'][i], eq['diff(theta)'](i))
+
+
+def dalamber_subs_nu(obj, eq):
+    for i in range(3):
+        obj = obj. \
+            subs(Derivative(psi[i], t, 2), eq['diff(psi)_nu'](i).diff(t)). \
+            subs(Derivative(psi[i], t, 2), eq['diff(psi)_nu'](i).diff(t)). \
+            subs(Derivative(psi[i], t), eq['diff(psi)_nu'](i)). \
+            subs(Derivative(theta[i], t), eq['diff(theta)_nu'](i))
+    return obj
---
+++ tatarinov/problems/triangular_platform/variables.py
@@ -0,0 +1,86 @@
+from sympy import symbols
+from sympy.physics.vector import dynamicsymbols
+
+
+# ИНИЦИАЛИЗАЦИЯ СИМВОЛОВ
+
+# Время
+t = symbols('t')
+# Индексы
+# i, P, C = symbols('i, P, C', cls=Idx) # i, P и С ???
+# (проекция вилки на e_z, проекция вилки на e_wheel, радиус, масса)
+h, d, r, R = symbols('h,d,r,R')
+# Тензор инерции
+# J = symbols('J') # J = IndexedBase('J')
+# Углы
+psi0, psi1, psi2 = dynamicsymbols('psi0, psi1, psi2')
+beta0, beta1, beta2 = symbols('beta0, beta1, beta2')
+theta0, theta1, theta2 = dynamicsymbols('theta0, theta1, theta2')
+psi = [psi0, psi1, psi2]
+beta = [beta0, beta1, beta2]
+theta = [theta0, theta1, theta2]
+
+#моменты инерции
+W1, W2, W3 = dynamicsymbols('W1, W2, W3'); T1, T2, T3 = dynamicsymbols('T1, T2, T3')
+T = {
+    1: T1,
+    2: T2,
+    3: T3
+}
+
+W = {
+    1: W1,
+    2: W2,
+    3: W3
+}
+# элементы матрицы моментов инерций для разных тел (ВРЕМЕННО)
+a,b,c = symbols('a,b,c')
+
+
+#СЛОВАРИ И ИХ СОДЕРЖИМОЕ  пример словаря: {'Алексей': 21}
+
+m = {}                                # словарь для масс
+e, omega, v = {}, {}, {}              # вектора, омега, скорость
+S, P, C, D  = {}, {}, {}, {}          # точки
+points = {}
+eq = {}                               # словарь со всякими выражениями
+delta = {};
+nu = {}                               # псевдоскорости nu[1] и nu[2]
+A = {}                                # части уравнения Д'Аламбера лагранжа для разных тел
+velocity = {}                         # скорость
+J = {}                                # моменты инерции для разных тел (осевые?)
+K = {}                                # кинетический момент для тела K = I_z*omega
+delta_r = {}                          # виртуальное перемещение для центра масс?
+M = {}                                # момент сил
+F = {}                                # силы, действующие на тело
+omega_delta = {}                      # виртуальны поворот
+coeff = {}                            # коэффиценты у Д'Аламюера-Лагранжа
+
+# Думаю здесь проблем не должно возникнуть, НО МАЛО ЛИ
+# Виртуальные перемещения
+delta['x'], delta['y'], delta['alpha'] = symbols('delta_x, delta_y, delta_alpha')
+delta_theta0, delta_theta1, delta_theta2 = dynamicsymbols('delta_theta0, delta_theta1, delta_theta2')
+delta_psi0, delta_psi1, delta_psi2 = dynamicsymbols('delta_psi0, delta_psi1, delta_psi2')
+delta['theta'] = [delta_theta0, delta_theta1, delta_theta2]
+delta['psi'] = [delta_psi0, delta_psi1, delta_psi2];
+# Псевдоскорости (взамен ẋ и ẏ)
+m['platform'], m['wheel'] = symbols('m1, m2')
+
+
+#ЗАВИСИМОСТИ
+x, y, alpha = dynamicsymbols('x, y, alpha')
+nu[1], nu[2], nu[3] = dynamicsymbols('nu1, nu2, nu3')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
