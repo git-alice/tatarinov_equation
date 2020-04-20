06a3c845070bf0937bcc4e6b9b694957d3dc1fe6
git-alice <art.oxbow@gmail.com>
2020-04-20 13:39:50 +0300

    Added apply_hack and Matrix for [T, W, \dot{\nu_3}]

--- tatarinov/utils/sympy.py
+++ tatarinov/utils/sympy.py
@@ -1,6 +1,28 @@
-from sympy import Matrix, Eq, Add
+from sympy import Matrix, Eq, Add, symbols
 from sympy.abc import t
 from typing import Sequence
+
+
+def _eqs_subs_vars_hack(eqs, vars):
+    tmp_eqs = []
+    tmp_vars = symbols(', '.join([f'tmp{i}' for i in range(len(vars))]))
+    sub_tmp_vars = {v: tv for v, tv in zip(vars, tmp_vars)}
+    for eq in eqs:
+        tmp_eqs.append(eq.subs(sub_tmp_vars))
+    return tmp_eqs, tmp_vars
+
+
+def apply_hack(func, eqs, vars):
+    tmp_eqs, tmp_vars = _eqs_subs_vars_hack(eqs, vars)
+    func_res = func(tmp_eqs, tmp_vars)
+    undo_subs_tmp_vars = {tv: v for v, tv in zip(vars, tmp_vars)}
+    try:
+        res = []
+        for i in range(len(func_res)):
+            res.append(func_res[i].subs(undo_subs_tmp_vars))
+    except:
+        res = func_res.subs(undo_subs_tmp_vars)
+    return res
 
 
 def MatrixElDerivative(mat: Matrix) -> Matrix:
