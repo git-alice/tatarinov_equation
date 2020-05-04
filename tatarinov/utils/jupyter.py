d874faf63c55479461e5f0111d7e2de6ffd29061
git-alice <art.oxbow@gmail.com>
2020-05-04 15:26:25 +0300

    Added display_from_what, display_list_from_what

--- tatarinov/utils/jupyter.py
+++ tatarinov/utils/jupyter.py
@@ -1,6 +1,7 @@
 from IPython.display import display, Markdown
 from typing import Any, Sequence
-
+from tatarinov.utils.sympy import from_what
+from sympy import Eq
 
 def display_list(l: Sequence[Any], spaces: bool = False):
     """
@@ -38,3 +39,28 @@ def debug_display(obj: Any = None, description: str = None) -> None:
     display(Markdown(f'__{description}__')) if description else None
     display(obj) if obj is not None else None
     display(Markdown('---'))
+
+
+def display_from_what(eq: Any) -> None:
+    """
+
+    @param eq:
+    @return:
+    """
+    if isinstance(eq, Eq):
+        display(eq.args[0])
+        print(' ↓ ')
+        display_list(from_what(eq.args[1]))
+    else:
+        display_list(from_what(eq))
+
+def display_list_from_what(eqs: Sequence[Any]) -> None:
+    """
+
+    @param eqs:
+    @return:
+    """
+
+    for eq in eqs:
+        display_from_what(eq)
+        display(Markdown('---'))
